fcabd6fc5c1608a6f60a7d0b4429de03411942b9
AyjayJr <ayjayjr@hotmail.com>
2023-07-24 23:16:03 -0400

    fixed next config / finished authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-	const isUserLoggedIn = true;
+	const { data: session } = useSession();
 	const [ providers, setProviders ] = useState(null);
-	const [toggleDropdown, setToggleDropdown] = useState(false);
+	const [ toggleDropdown, setToggleDropdown ] = useState(false);
 
 	useEffect(() => {
-		const setProviders = async () => {
+		const setUpProviders = async () => {
 			const response = await getProviders();
 
 			setProviders(response);
 		}
 
-		setProviders();
+		setUpProviders();
 	}, [])
 
 	return (
@@ -35,7 +35,7 @@ const Nav = () => {
 
 			{/* Desktop navigation */}
 			<div className="sm:flex hidden">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex gap-3 md:gap-5">
 						<Link
 							href="/create-prompt"
@@ -65,7 +65,7 @@ const Nav = () => {
 				) : (
 					<>
 						{providers &&
-							Object.values(providers).map((provider) => {
+							Object.values(providers).map((provider) => (
 								<button
 									type="button"
 									key={provider.name}
@@ -73,8 +73,8 @@ const Nav = () => {
 									className='black_btn'
 								>
 									Sign In 
-								</button>
-							})
+								</button>	
+							))
 						}
 					</>
 				)}
@@ -82,10 +82,10 @@ const Nav = () => {
 
 			{/* Mobile Navigation */}
 			<div className="sm:hidden flex relative">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex">
 						<Image 
-							src="/assets/images/logo.svg"
+							src={session?.user.image}
 							width={37}
 							height={37}
 							className="rounded-full"
