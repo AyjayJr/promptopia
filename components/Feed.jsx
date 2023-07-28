f47c0f89f3b25bac2407ed136f3134b97a77d701
AyjayJr <ayjayjr@hotmail.com>
2023-07-27 23:54:46 -0400

    reverting to state of the project at 948a4f1

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,7 +2,21 @@
 
 import { useState, useEffect }from 'react';
 
-import PromptCardList from './PromptCardList';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map((post) => (
+				<PromptCard
+					key={post._id}
+					post={post}
+					handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	)
+}
 
 const Feed = () => {
 	const [searchText, setSearchText] = useState("");
