b3387304b8fa11fc8af9a8a19efa33906b74e0fa
AyjayJr <ayjayjr@hotmail.com>
2023-07-28 17:44:27 -0400

    fixed bug causing error when trying to get prompts by id

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const EditPrompt = () => {
 	const router = useRouter();
-	const { data: session } = useSession();
+	const searchParams = useSearchParams();
+	const promptId = searchParams.get('id');
 
 	const [submitting, setSubmitting] = useState(false);
 	const [post, setPost] = useState({
@@ -16,29 +17,45 @@ const EditPrompt = () => {
 		tag: ""
 	});
 
-	const createPrompt = async (e) => {
-		e.preventDefault();
-		setSubmitting(true);
+	useEffect(() => {
+		const getPromptDetails = async () => {
+			const response = await fetch(`/api/prompt/${promptId}`);
+			const data = await response.json();
 
-		try {
-			const response = await fetch('/api/prompt/new', {
-				method: "POST",
-				body: JSON.stringify({
-					prompt: post.prompt,
-					userId: session?.user.id,
-					tag: post.tag
-				})
-			});
-
-			if (response.ok) {
-				router.push('/');
-			}
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setSubmitting(false);
+			setPost({
+				prompt: data.prompt,
+				tag: data.tag
+			})
 		}
-	}
+
+		if (promptId) {
+			getPromptDetails();
+		}
+	}, [promptId])
+
+	// const createPrompt = async (e) => {
+	// 	e.preventDefault();
+	// 	setSubmitting(true);
+
+	// 	try {
+	// 		const response = await fetch('/api/prompt/new', {
+	// 			method: "POST",
+	// 			body: JSON.stringify({
+	// 				prompt: post.prompt,
+	// 				userId: session?.user.id,
+	// 				tag: post.tag
+	// 			})
+	// 		});
+
+	// 		if (response.ok) {
+	// 			router.push('/');
+	// 		}
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	} finally {
+	// 		setSubmitting(false);
+	// 	}
+	// }
 
 	return (
 		<Form 
@@ -46,7 +63,7 @@ const EditPrompt = () => {
 			post={post}		
 			setPost={setPost}
 			submitting={submitting}
-			handleSubmit={createPrompt}
+			handleSubmit={() => {}}
 		/>
 	)
 }
