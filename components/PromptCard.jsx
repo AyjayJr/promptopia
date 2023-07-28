7d04049add2962c665c40edae51ae49cd89ca6e8
AyjayJr <ayjayjr@hotmail.com>
2023-07-27 22:01:03 -0400

    fixed copy icon not switching to tick

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -36,12 +36,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 					</div>
 				</div>
 
-				<div className="copy_btn" onClick={() => {handleCopy}}>
+				<div className="copy_btn" onClick={handleCopy}>
 					<Image
 						src={copied === post.prompt
 							? '/assets/icons/tick.svg'
 							: '/assets/icons/copy.svg'
 						}
+						alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
 						width={12}
 						height={12}
 					/>
