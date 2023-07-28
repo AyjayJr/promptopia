6a99e3773decc86619c78be748055190ac970c9c
AyjayJr <ayjayjr@hotmail.com>
2023-07-28 18:14:14 -0400

    completed all major CRUD applications

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -30,7 +30,21 @@ const MyProfile = () => {
 		router.push(`/update-prompt?id=${post._id}`);
 	}
 
-	const handleDelete =(post) => {
+	const handleDelete = async (post) => {
+		const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: "DELETE"
+				});
+
+				const filteredPosts = posts.filter((p) => p._id !== post._id);
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
 	}
 
@@ -40,7 +54,7 @@ const MyProfile = () => {
 			desc="Welcome to your personalized profile page"
 			data={posts}
 			handleEdit={handleEdit}
-			handleDeleter={handleDelete}
+			handleDelete={handleDelete}
 		/>
 	)
 }
